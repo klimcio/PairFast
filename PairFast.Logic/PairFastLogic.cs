c5e2187f28d6807fea9d1bd733a2e89cbe71b3e2
Klimek, Mariusz <klimcio@gmail.com>
2024-02-07 22:39:14 +0100

    Create the simplest structure for the entire process, no code, all throw exceptions, all classes are empty
    NEXT: Unit Tests for common logic

---
+++ PairFast.Logic/PairFastLogic.cs
@@ -0,0 +1,19 @@
+﻿namespace PairFast.Logic;
+
+public static class PairFastLogic
+{
+    public static IEnumerable<Pair> PairItems(IEnumerable<string> items)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Pair
+{
+
+}
+
+public class PairCompared
+{
+
+}
--- PairFast.PrototypeConsoleApp/Program.cs
+++ PairFast.PrototypeConsoleApp/Program.cs
@@ -1,9 +1,46 @@
-﻿namespace PairFast.PrototypeConsoleApp;
+﻿
+using PairFast.Logic;
+
+namespace PairFast.PrototypeConsoleApp;
 
 internal class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        Extensions.CollectItems()
+            .PairItems()
+            .CompareItems()
+            .InterpretResults()
+            .ViewResults();
     }
+
+}
+
+internal static class Extensions
+{
+    public static IEnumerable<string> CollectItems()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static IEnumerable<Pair> PairItems(this IEnumerable<string> items) 
+        => PairFastLogic.PairItems(items);
+
+    public static IEnumerable<PairCompared> CompareItems(this IEnumerable<Pair> pairs)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static IDictionary<string, int> InterpretResults(this IEnumerable<PairCompared> pairs)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void ViewResults(this IDictionary<string, int> pairs)
+    {
+        throw new NotImplementedException();
+    }
+
 }
